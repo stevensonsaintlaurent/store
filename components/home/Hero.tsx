4bd5303a8f78b0ab23c783006b97944c4691bd5b
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-08-11 23:17:33 +0300

    react hero component

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import HeroCarousel from "./HeroCarousel";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24">
+      <div>
+        <h2 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+          We are changing the way people shop
+        </h2>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi
+          consequatur magnam iure repudiandae nihil cumque illum, repellat
+          accusantium. Soluta eligendi necessitatibus consequatur amet excepturi
+          architecto rem quam blanditiis eos alias, cum earum vitae impedit
+          error debitis vero! Quibusdam, voluptatem eum.
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products">Our Products</Link>
+        </Button>
+      </div>
+
+      <HeroCarousel />
+    </section>
+  );
 };
 
 export default Hero;
