4e4883a840f202773b36291ee353ec4fdc9a94a9
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-08-12 11:29:17 +0300

    herocarouselt

--- components/home/HeroCarousel.tsx
+++ components/home/HeroCarousel.tsx
@@ -1,7 +1,43 @@
-import React from "react";
-
+import hero1 from "@/public/images/hero1.jpg";
+import hero2 from "@/public/images/hero2.jpg";
+import hero3 from "@/public/images/hero3.jpg";
+import hero4 from "@/public/images/hero4.jpg";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import Image from "next/image";
+import { Card, CardContent } from "../ui/card";
+const caroulseImages = [hero1, hero2, hero3, hero4];
 const HeroCarousel = () => {
-  return <div>hero carousel</div>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel>
+        <CarouselContent>
+          {caroulseImages.map((image, index) => {
+            return (
+              <CarouselItem key={index}>
+                <Card>
+                  <CardContent>
+                    <Image
+                      src={image}
+                      alt="hero"
+                      className="w-full h-[24rem] rounded-md object-cover"
+                    />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 };
 
 export default HeroCarousel;
