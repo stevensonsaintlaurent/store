e04e6397167b72b0145c5d0945e46bccd8479524
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-08-19 01:24:08 +0300

    i still habe a bugs

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
-const ProductRating = () => {
-  return <div>product rating</div>;
+const ProductRating = ({ productId }: { productId: string }) => {
+  const rating = 4.5;
+  const count = 25;
+  const className = `flex gap-1 items-center text-md mt-1 mb-4`;
+  const countValue = `(${count}) reviews`;
+  return (
+    <span className={className}>
+      <FaStar className="w-3 h-3" />
+      {rating} {countValue}
+    </span>
+  );
 };
 
 export default ProductRating;
