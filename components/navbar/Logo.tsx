643b9c2ea61157ff2c5066b5eac372c08aebcf7b
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-08-22 12:12:11 +0300

    bugs some where i coudnt find

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -5,7 +5,7 @@ import { VscCode } from "react-icons/vsc";
 
 const Logo = () => {
   return (
-    <Button size="icon" aschild="true">
+    <Button size="icon" asChild="true">
       <Link href="/">
         <VscCode className="w-6 h-6" />
       </Link>
