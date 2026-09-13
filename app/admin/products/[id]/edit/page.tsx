46529fa0150220ada1ed05fcce1c655bcba747dd
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-09-14 01:18:59 +0300

    i have a bugs

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,10 +1,54 @@
-import React from "react";
+import { fetchAdminProductDetails, updateProductAction } from "@/utils/actions";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import PriceInput from "@/components/form/PriceInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import { SubmitButton } from "@/components/form/Buttons";
+import CheckboxInput from "@/components/form/CheckboxInput";
+import { defineConfig } from "prisma/config";
 
-const EditProductPage = () => {
+const EditProductPage = async ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const product = await fetchAdminProductDetails(id);
+  const { name, company, description, featured, price } = product;
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Edit Product</h1>
-    </div>
+    <section>
+      <h1 className="text-2xl font-semibold mb-8 capitalize">Update Product</h1>
+      <div className="border p-8 rounded">
+        {/* IMAGE INPUT CONTAINER */}
+        <FormContainer action={updateProductAction}>
+          <div className="grid gap-4 md:grid-cols-2 my-4">
+            <input type="hidden" name="id" value={id} />
+
+            <FormInput
+              type="text"
+              name="name"
+              label="product name"
+              defaultValue={name}
+            />
+
+            <FormInput type="text" name="company" defaultValue={company} />
+
+            <PriceInput defaultValue={price} />
+          </div>
+
+          <TextAreaInput
+            name="description"
+            labelText="product description"
+            defaultValue={description}
+          />
+
+          <div className="mt-6">
+            <CheckboxInput
+              name="featured"
+              label="featured"
+              defaultChecked={featured}
+            />
+          </div>
+        </FormContainer>
+      </div>
+    </section>
   );
 };
 
