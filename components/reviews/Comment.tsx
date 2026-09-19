7ab66806fb3fa6f18284151048d7c4ea71c6938b
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-09-19 14:44:34 +0300

    comment

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+"use client";
 
-const Comment = () => {
-  return <div>comment</div>;
+import { useState } from "react";
+import { Button } from "../ui/button";
+
+const Comment = ({ comment }: { comment: string }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  const longComment = comment.length > 130;
+  const displayComment =
+    longComment && !isExpanded ? `${comment.slice(0, 130)}...` : comment;
+  return (
+    <div>
+      <p className="text-sm">{displayComment}</p>
+      {longComment && (
+        <Button
+          variant="link"
+          className="pl-0 text-muted-foreground"
+          onClick={toggleExpanded}
+        >
+          {isExpanded ? "Show Less" : "Show More"}
+        </Button>
+      )}
+    </div>
+  );
 };
 
 export default Comment;
