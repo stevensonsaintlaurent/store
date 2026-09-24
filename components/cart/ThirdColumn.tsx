a3be09d4c634f3cca459b507e18059c15bc56e0a
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-09-24 21:00:19 +0300

    finish the projet

--- components/cart/ThirdColumn.tsx
+++ components/cart/ThirdColumn.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import SelectProductAmount from "../single-product/SelectProductAmount";
+import { Mode } from "../single-product/SelectProductAmount";
+import FormContainer from "../form/FormContainer";
+import { SubmitButton } from "../form/Buttons";
+import { removeCartItemAction, updateCartItemAction } from "@/utils/actions";
+import { Toast } from "../ui/toast";
 
-const ThirdColumn = () => {
-  return <div>third column</div>;
+const ThirdColumn = ({ quantity, id }: { quantity: number; id: string }) => {
+  const [amount, setAmount] = useState(quantity);
+  const handleAmountChange = async (value: number) => {
+    setAmount(value);
+  };
+  return (
+    <div className="md:ml-8">
+      <SelectProductAmount
+        amount={amount}
+        setAmount={handleAmountChange}
+        mode={Mode.CartItem}
+        isLoading={false}
+      />
+
+      <FormContainer action={removeCartItemAction}>
+        <input type="hidden" name="id" value={id} />
+        <SubmitButton size="sm" className="mt-4" text="remove" />
+      </FormContainer>
+    </div>
+  );
 };
 
 export default ThirdColumn;
--- utils/actions.ts
+++ utils/actions.ts
@@ -525,7 +525,30 @@ export const addToCartAction = async (prevState: any, formData: FormData) => {
   }
 };
 
-export const removeCartItemAction = async () => {};
+export const removeCartItemAction = async (
+  prevState: any,
+  formData: FormData,
+) => {
+  const user = await getAuthUser();
+  try {
+    const cartItemId = formData.get("id") as string;
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+    await db.cartItem.delete({
+      where: {
+        id: cartItemId,
+        cartId: cart.id,
+      },
+    });
+    await updateCart(cart);
+    revalidatePath("/cart");
+    return { message: "Item removed from cart" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
 
 export const updateCartItemAction = async () => {};
 
