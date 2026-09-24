4f774764e39bf6403109db7a73a819dae5712a61
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-09-24 11:32:19 +0300

    finish the project with no bugs

--- components/cart/CartItemsList.tsx
+++ components/cart/CartItemsList.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Card } from "@/components/ui/card";
+import { FirstColumn, SecondColumn, FourthColumn } from "./CartItemColumns";
+import ThirdColumn from "./ThirdColumn";
+import { CartItemWithProduct } from "@/utils/types";
 
-const CartItemsList = () => {
-  return <div>CartItemsList</div>;
+const CartItemsList = ({ cartItems }: { cartItems: CartItemWithProduct[] }) => {
+  return (
+    <div>
+      {cartItems.map((cartItem) => {
+        const { id, amount } = cartItem;
+        const { image, name, company, price, id: productId } = cartItem.product;
+        return (
+          <Card
+            key={id}
+            className="flex flex-col gap-y-4 md:flex-row flex-wrap p-6 mb-8 gap-x-4"
+          >
+            <FirstColumn image={image} name={name} />
+            <SecondColumn company={company} name={name} productId={id} />
+            <ThirdColumn id={id} quantity={amount} />
+            <FourthColumn price={price} />
+          </Card>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CartItemsList;
