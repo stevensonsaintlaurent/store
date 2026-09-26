6101ddfcbd5ab7789cf8f0dfa6b447e3ac009f1c
Stevenson St Laurent <stevensonstlaurent08@gmail.com>
2026-09-26 21:11:59 +0300

    finish this part

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
-const OrdersPage = () => {
+import SectionTitle from "@/components/global/SectionTitle";
+import { fetchUserOrders } from "@/utils/actions";
+import { formatCurrency, formatDate } from "@/utils/format";
+
+const OrdersPage = async () => {
+  const orders = await fetchUserOrders();
   return (
-    <div>
-      orders page
-    </div>
-  )
-}
+    <>
+      <SectionTitle text="Your Orders" />
 
-export default OrdersPage
+      <Table>
+        <TableCaption>Total Orders : {orders.length}</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Products</TableHead>
+            <TableHead>Order Total</TableHead>
+            <TableHead>Tax</TableHead>
+            <TableHead>Shipping</TableHead>
+            <TableHead>Date</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {orders.map((order) => {
+            const { id, products, orderTotal, tax, shipping, createdAt } =
+              order;
+            return (
+              <TableRow key={id}>
+                <TableCell>{products}</TableCell>
+                <TableCell>{formatCurrency(orderTotal)}</TableCell>
+                <TableCell>{formatCurrency(tax)}</TableCell>
+                <TableCell>{formatCurrency(shipping)}</TableCell>
+                <TableCell>{formatDate(createdAt)}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
+};
+
+export default OrdersPage;
